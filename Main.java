e91186a5ef7204cb8afaa2d1778e4d20a4a1f886
chief <69991451+Josh-chief@users.noreply.github.com>
2020-11-10 16:13:45 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Collage student1=new Collage();
+
+
+
+      ;
+        student1.data_in();
+        student1.data_out();
+        //student1.compute();
+
+
+
+
+       /* System.out.println(student1.programming);
+        System.out.println(student1.web_design);
+        System.out.println(student1.networking);*/
+
+        //student1.total();
+
+
+    }
+}
